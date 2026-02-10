2000379dcb0675da07e052e8bb3ef953e686d4d7
memoryleak47 <memoryleak47@protonmail.ch>
2026-02-10 17:01:04 +0100

    ...

--- src/flatterm.rs
+++ src/flatterm.rs
@@ -1,9 +1,19 @@
 use crate::*;
 
-#[derive(Clone, Copy, PartialOrd, Ord, PartialEq, Eq, Debug)]
+#[derive(Clone, Copy, PartialOrd, Ord, PartialEq, Eq)]
 pub struct Entry {
     pub sym: Sym,
     pub size: u32, // size of that subterm
 }
 
 pub type FlatTerm = [Entry];
+
+use std::fmt::*;
+
+impl Display for Entry {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "({}: {})", self.sym, self.size)
+    }
+}
+
+impl Debug for Entry { fn fmt(&self, f: &mut Formatter<'_>) -> Result { write!(f, "{self}") } }
