11e884c87a3766e734eb483843cb582fe62dde7e
memoryleak47 <memoryleak47@protonmail.ch>
2026-02-12 17:48:54 +0100

    impl first tet for TermIndex

--- src/index.rs
+++ src/index.rs
@@ -26,32 +26,64 @@ impl<T> TermIndex<T> {
         index.here.push(t);
     }
 
-    pub fn index_match(&self, k: &FlatTerm) -> Vec<(Subst, &T)> {
+    pub fn find_matches(&self, k: &FlatTerm) -> Vec<(Subst, &T)> {
         assert!(is_canon_term(k));
 
         if k.is_empty() {
             return self.here.iter().map(|x| (Subst::new(), x)).collect()
         }
 
-        // should this also happen if a.is_var()?
+        let mut out: Vec<(Subst, &T)> = Vec::new();
+
         let a = k[0].sym;
         if !a.is_var() && let Some(m) = self.children.get(&a) {
-            // TODO fix substs.
-            return m.index_match(&k[1..]);
+            out.extend(m.find_matches(&k[1..]));
         }
 
         for (s, idx) in &self.children {
             if !s.is_var() { continue }
 
-            // TODO ...
+            let size = k[0].size as usize;
+            let a = &k[0..size];
+            let b = &k[size..];
+            for (mut subst, t) in idx.find_matches(b) {
+                subst.insert(*s, ft_box(a));
+                out.push((subst, t));
+            }
         }
 
-        todo!()
+        out
     }
 
-    pub fn index_lookup(&self, k: &FlatTerm) -> Option<Vec<T>> {
+    pub fn lookup(&self, k: &FlatTerm) -> Option<Vec<T>> {
         assert!(is_canon_term(k));
 
         todo!()
     }
+}
+
+#[test]
+fn test_index() {
+    let mut index = TermIndex::new();
+    let t_in = [
+        Entry { sym: Sym::mk_fn(0), size: 3 },
+        Entry { sym: Sym::mk_var(0), size: 1 },
+        Entry { sym: Sym::mk_var(0), size: 1 },
+    ];
+    index.add(&t_in, 0);
+    let t_out = [
+        Entry { sym: Sym::mk_fn(0), size: 5 },
+        Entry { sym: Sym::mk_fn(1), size: 2 },
+        Entry { sym: Sym::mk_fn(2), size: 1 },
+        Entry { sym: Sym::mk_fn(1), size: 2 },
+        Entry { sym: Sym::mk_fn(2), size: 1 },
+    ];
+    let repl: Box<FlatTerm> = Box::new([
+        Entry { sym: Sym::mk_fn(1), size: 2 },
+        Entry { sym: Sym::mk_fn(2), size: 1 },
+    ]);
+    let subst: Subst = [(Sym::mk_var(0), repl)].into_iter().collect();
+    let matches: Vec<(Subst, &usize)> = index.find_matches(&t_out);
+    let other: Vec<(Subst, &usize)> = vec![(subst, &0)];
+    assert_eq!(matches, other);
 }
